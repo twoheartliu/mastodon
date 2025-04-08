e27522d75512f64f859a1f8e7dba2fd3c1e53a73
twoheart <twoheart@vip.qq.com>
2025-04-08 22:45:11 +0800

    fix

--- app/javascript/mastodon/features/ui/index.jsx
+++ app/javascript/mastodon/features/ui/index.jsx
@@ -566,7 +566,7 @@ class UI extends PureComponent {
     const { children, isComposing, location, layout } = this.props;
 
     // 计算singleColumn，同时考虑layout和layoutType的影响
-    const isSingleColumn = layout === 'mobile' || layoutType !== 'advance';
+    const isSingleColumn = layout === 'mobile' || layoutType !== 'advanced';
 
     console.log('layoutType', layoutType);
     console.log('layout', layout);
